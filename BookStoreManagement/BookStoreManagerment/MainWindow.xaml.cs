3bb850f6d9bdebe5d29e06827d3db32dadf13110
SyaoranClone <37147552+SyaoranClone@users.noreply.github.com>
2018-03-15 01:23:17 +0700

    Bookstore management main display

---
+++ BookStoreManagement/BookStoreManagerment/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Ribbon;
+
+namespace BookStoreManagerment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public List<AppInfo> AppInfos { get { return BookStoreManagerment.AppInfos.listInfo; } }
+    }
+    public class AppInfo
+    {
+        public string Content { get; set; }
+        public string Photo { get; set; }
+        public ImageSource PhotoSource
+        {
+            get { return string.IsNullOrEmpty(Photo) ? null : new BitmapImage(new Uri(Photo, UriKind.Relative)); }
+        }
+    }
+    public static class AppInfos
+    {
+        public static readonly List<AppInfo> listInfo = new List<AppInfo>
+        {
+            new AppInfo {Content = "Bookstore management application by Evils group",Photo = "Images/background4.jpg" },
+            new AppInfo {Content = "For more infomation vist our Github",Photo = "Images/background5.jpg" },
+            new AppInfo {Content = "",Photo="Images/background3.jpg" }
+        };
+    }
+}
