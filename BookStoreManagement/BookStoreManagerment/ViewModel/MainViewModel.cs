8e573bb61ae26ae71c82c5dd0e7990c69d3911d5
SyaoranClone <37147552+SyaoranClone@users.noreply.github.com>
2018-03-24 16:12:37 +0700

    Add Login form into project

---
+++ BookStoreManagement/BookStoreManagerment/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BookStoreManagerment.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+
+        // mọi thứ xử lý sẽ nằm trong này
+        public MainViewModel()
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            //LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                Isloaded = true;
+            //    LoginWindow loginWindow = new LoginWindow();
+            //    loginWindow.ShowDialog();
+            //}
+            //  );
+        }
+        //All commands
+        public List<AppInfo> AppInfos { get { return BookStoreManagerment.ViewModel.AppInfos.listInfo; } }
+    }
+
+    #region AppInformation
+    public class AppInfo
+    {
+        public string Content { get; set; }
+        public string Photo { get; set; }
+        public ImageSource PhotoSource
+        {
+            get { return string.IsNullOrEmpty(Photo) ? null : new BitmapImage(new Uri(Photo, UriKind.Relative)); }
+        }
+    }
+    public static class AppInfos
+    {
+        public static readonly List<AppInfo> listInfo = new List<AppInfo>
+        {
+            new AppInfo {Content = "Bookstore management application by Evils group",Photo = "Images/background4.jpg" },
+            new AppInfo {Content = "For more infomation vist our Github",Photo = "Images/background5.jpg" },
+            new AppInfo {Content = "",Photo="Images/background3.jpg" }
+        };
+    }
+    #endregion
+}
